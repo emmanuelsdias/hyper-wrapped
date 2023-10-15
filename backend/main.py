b7e2644ce5caeb6afe5df770a9fed4b92868bf8e
Emmanuel <emanusdias@gmail.com>
2023-10-15 01:11:45 -0300

    changed default backend ip and added CORS

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ import sys
 sys.dont_write_bytecode = True # prevents __pycache__ folders
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import Base, engine
 from routes import user, trend
@@ -9,6 +10,14 @@ from routes import user, trend
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router, prefix="/user", tags=["User"])
 app.include_router(trend.router, prefix="/trend", tags=["Trend"])
