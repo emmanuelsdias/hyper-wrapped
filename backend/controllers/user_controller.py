4a9f353e4e15412101e8bfc907c2f180edfa8147
Emmanuel <emanusdias@gmail.com>
2023-10-15 01:12:10 -0300

    added dummy login support (no password)

--- backend/controllers/user_controller.py
+++ backend/controllers/user_controller.py
@@ -2,7 +2,7 @@ from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from models.user import User
-from schemas.user import UserBase
+from schemas.user import UserBase, UserSchema
 
 
 def get_user_information(db: Session, user_id: int) -> UserBase:
@@ -21,5 +21,25 @@ def get_user_information(db: Session, user_id: int) -> UserBase:
         username=user.username,
         first_name=user.first_name,
         last_name=user.last_name
+    )
+    return user_info
+
+# Dummy login function, since it shouldn't require username only
+def login_with_username(db: Session, username: str) -> UserSchema:
+    user = (
+        db.query(User)
+        .filter(User.username == username)
+        .first()
+    )
+    # If user is not found, raise an exception
+    if not user:
+        raise HTTPException(status_code=404, detail="Username not found")
+    
+    # Create a UserSchema object and return
+    user_info = UserSchema(
+        username=user.username,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        user_id=user.user_id
     )
     return user_info
--- backend/routes/user.py
+++ backend/routes/user.py
@@ -3,10 +3,11 @@ from sqlalchemy.orm import Session
 
 from controllers import user_controller
 from database.db import get_db
-from schemas.user import UserBase
+from schemas.user import UserBase, UserSchema
 
 
 router = APIRouter()
+
 
 @router.get("/{user_id}", response_model=UserBase)
 def get_user_info(
@@ -14,3 +15,11 @@ def get_user_info(
     db: Session = Depends(get_db)
 ):
     return user_controller.get_user_information(db, user_id)
+
+
+@router.post("/login/{username}", response_model=UserSchema)
+def user_login(
+    username: str,
+    db: Session = Depends(get_db)
+):
+    return user_controller.login_with_username(db, username)
